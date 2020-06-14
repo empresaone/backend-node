ecdd9a359a3539291ca8dccf0bab3a0cf8d3a0e5
Jesus Finol <jfinol02@gmail.com>
2020-06-14 15:32:56 -0400

    Login con google

--- app/config/config.js
+++ app/config/config.js
@@ -2,3 +2,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.URI_BD = process.env.URI_BD || 'mongodb://localhost:27017/comuna';
 process.env.SEED = process.env.SEED || 'kjdfbkgr8384p3it3{t';
 process.env.EXPIRE = process.env.EXPIRE || '1h';
+process.env.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '56523024150-4euspdt6r3uhhujna20f3drv3p30f91v.apps.googleusercontent.com';
--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,8 @@ const app = express();
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 app.post('/login', (req, res) => {
 
@@ -44,6 +46,100 @@ app.post('/login', (req, res) => {
             token: token
         });
     });
+});
+
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    console.log(payload.name);
+    console.log(payload.email);
+    console.log(payload.picture);
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    let token = req.body.idtoken;
+    let googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                err: e
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioBD) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        if (usuarioBD) {
+            if (usuarioBD.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe ingresar mediante el formulario de login'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioBD
+                }, process.env.SEED, {
+                    expiresIn: process.env.EXPIRE
+                });
+
+                return res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token
+                });
+            }
+        } else {
+
+            let usuario = new Usuario();
+            usuario.name = googleUser.name;
+            usuario.email = googleUser.email;
+            usuario.image = googleUser.img;
+            usuario.google = true;
+            usuario.password = '**';
+
+            usuario.save((err, usuarioBD) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err: err
+                    });
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioBD
+                }, process.env.SEED, {
+                    expiresIn: process.env.CADUCIDAD_TOKEN
+                });
+
+                return res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token
+                });
+            });
+        }
+    })
 });
 
 module.exports = app;
